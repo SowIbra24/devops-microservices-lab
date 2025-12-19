ebd68a4c1069f6b72405f9c5e71590b9e5ac1bf6
p2019597 <ibrahima.sow@etu.univ-lyon1.fr>
2025-12-19 17:21:16 +0100

    fichier runner qui sera appelé dans mon gitlab_ci et mon github_action pour garder là meme logique de tests

---
+++ ci/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "....... Test de la calculatrice ................. "
+
+chmod +x ci/test_curl_calculatrice.sh
+ci/test_curl_calculatrice.sh
+if [ $? -ne 0 ]; then
+    echo "Le test de la calculatrice a échoué, arrêt du runner"
+    exit 1
+fi
+
+echo "....... Fin du test de la calculatrice .......... "
+
+echo "....... Test manuel avec curl de la valeur du compteur ........ "
+
+chmod +x ci/test_curl_app_flask.sh
+ci/test_curl_app_flask.sh
+if [ $? -ne 0 ]; then
+    echo "Le test du compteur a échoué, arrêt du runner"
+    exit 1
+fi
+
+echo "........ Fin des tests manuels avec curl ...................... "
