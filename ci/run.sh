8cec546a5a6f159bba53a4dd89d3adb4544fef78
p2019597 <ibrahima.sow@etu.univ-lyon1.fr>
2025-12-22 13:58:08 +0100

    test avec installation de bash

--- ci/run.sh
+++ ci/run.sh
@@ -2,8 +2,8 @@
 
 echo "....... Test de la calculatrice ................. "
 
-chmod +x ./test_curl_calculatrice.sh
-./test_curl_calculatrice.sh
+chmod +x ci/test_curl_calculatrice.sh
+ci/test_curl_calculatrice.sh
 if [ $? -ne 0 ]; then
     echo "Le test de la calculatrice a échoué, arrêt du runner"
     exit 1
@@ -15,8 +15,8 @@ sleep 5 # au cas où la base de données ne serait pas prete
 
 echo "....... Test manuel avec curl de la valeur du compteur ........ "
 
-chmod +x ./test_curl_app_flask.sh
-./test_curl_app_flask.sh
+chmod +x ci/test_curl_app_flask.sh
+ci/test_curl_app_flask.sh
 if [ $? -ne 0 ]; then
     echo "Le test du compteur a échoué, arrêt du runner"
     exit 1
